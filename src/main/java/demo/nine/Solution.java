769d1f4f7fb6a9af59c072bda9d3d97fc7e8dc68
zhouxiaogang <zhou16386@163.com>
2020-07-25 23:40:24 +0800

    temp save

---
+++ src/main/java/demo/nine/Solution.java
@@ -0,0 +1,13 @@
+package demo.nine;
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+        int halfReverseX = 0;
+        while (x > halfReverseX) {
+            halfReverseX = halfReverseX * 10 + x % 10;
+            x /= 10;
+        }
+        return halfReverseX == x || halfReverseX / 10 == x;
+    }
+}
